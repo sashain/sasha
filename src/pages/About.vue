0ff2961487d3d63eaafce640002d1a81b9d1bc92
Keerti <keerti@apple.com>
2021-01-06 15:17:45 -0800

    added about content

--- src/pages/About.vue
+++ src/pages/About.vue
@@ -1,7 +1,11 @@
 <template>
   <Layout>
-    <h1>About us</h1>
-    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Error doloremque omnis animi, eligendi magni a voluptatum, vitae, consequuntur rerum illum odit fugit assumenda rem dolores inventore iste reprehenderit maxime! Iusto.</p>
+    <h1>ABOUT</h1>
+    <p>SASHA stands for Support Against Sexual HArassment. We mean all things PoSH.</p>
+    <p>At SASHA, we strive to create safe and inclusive workplaces. Not just because the law mandates it. But because every employee deserves to work at a place that makes them feel safe, secure, heard and respected.</p>
+    <p>Since its foundation in 2012, SASHA’s team, comprised of experienced legal consultants and a psychological counsellor have stayed dedicated to the cause of helping organisations understand not just the word, but the spirit, that results in the effective implementation of the Sexual Harassment of Women at Workplace (Prevention, Prohibition and Redressal) Act 2013. </p>
+    <p>What began as a mere discussion between two friends, Founder Kanti Joshi and Co-founder Sowmya Bhat, has grown into a proud organisation that exclusively provides PoSH services today. Our innovative approach focuses on effective awareness, sensitization and training. From drafting the PoSH policy to ensuring unbiased inquiries into complaints and providing timely grievance redressal, SASHA provides handholding support at every step of the way.
+    </p>
   </Layout>
 </template>
 
---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors:{
+        red:'#ee3022',
+        black:'#0D131A'
+      },
+      fontFamily: {
+        sans:['Poppins']
+      }
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
