856d2091612ee10f22948269144b375e4c7fa821
Keerti <keerti@apple.com>
2021-08-04 17:02:05 -0700

    fixing tailwind import

--- gridsome.config.js
+++ gridsome.config.js
@@ -40,6 +40,9 @@ module.exports = {
       use: "gridsome-plugin-tailwindcss",
       options: {
         tailwindConfig: './tailwind.config.js',
+        purgeConfig: {},
+        presetEnvConfig: {},
+        shouldPurge: true,
         shouldImport: true,
         shouldTimeTravel: true,
       },
